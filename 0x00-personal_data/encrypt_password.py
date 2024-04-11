9fcbda4c3e8d7e4ed93898ae170c233dce2a68b6
abu al ameen <abulyaqs@gmail.com>
2024-04-11 23:38:49 +0100

    task last

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,14 +1,20 @@
+#!/usr/bin/env python3
+
+
 import bcrypt
+
 
 def hash_password(password: str) -> bytes:
     """Hash the provided password using bcrypt."""
     # Generate a salt and hash the password using bcrypt
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if the provided password matches the hashed password."""
     # Use bcrypt to verify the password against the hashed password
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
 
 if __name__ == "__main__":
     password = "MyAmazingPassw0rd"
