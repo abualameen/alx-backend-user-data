9eefcab9ceeae301f3cb9df3ab5e89932139d471
abu al ameen <abulyaqs@gmail.com>
2024-04-11 21:50:50 +0100

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,56 +1,10 @@
 #!/usr/bin/env python3
+"""
+Filtered Logger Module
+"""
 
-import logging
 import re
 from typing import List
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ Format method to filter values in incoming log records """
-        for field in self.fields:
-            pattern = re.escape(field) + r'=[^' + \
-                re.escape(self.SEPARATOR) + r']+'
-            record.msg = re.sub(
-                pattern, field + '=' + self.REDACTION, record.msg)
-        return super().format(record)
-
-
-# Define the PII_FIELDS constant
-PII_FIELDS = ("field1", "field2", "field3", "field4", "field5")
-
-
-def get_logger() -> logging.Logger:
-    """Create and configure a logger named 'user_data'."""
-    # Create a new logger named 'user_data'
-    logger = logging.getLogger("user_data")
-
-    # Set the logging level to INFO
-    logger.setLevel(logging.INFO)
-
-    # Prevent messages from being propagated to other loggers
-    logger.propagate = False
-
-    # Create a StreamHandler
-    stream_handler = logging.StreamHandler()
-
-    # Set the formatter for the StreamHandler to RedactingFormatter
-    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
-
-    # Add the StreamHandler to the logger
-    logger.addHandler(stream_handler)
-
-    return logger
 
 
 def filter_datum(fields: List[str], redaction: str,
