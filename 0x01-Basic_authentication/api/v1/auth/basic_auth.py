a09b89f67df97e509db8dd8d06f19e09b77af84a
abu al ameen <abulyaqs@gmail.com>
2024-04-16 21:37:25 +0100

    10 task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,10 +4,11 @@ this module is the basic auth class
 
 """
 
-
+from models.user import User
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -52,3 +53,16 @@ class BasicAuth(Auth):
         user_email, user_password = decoded_base64_authorization_header.split(
            ':', 1)
         return user_email, user_password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """this function checks user obj from creden"""
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+        user_list = User.search({'email': user_email})
+        if not user_list:
+            return None
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
