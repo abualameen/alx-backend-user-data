812d756bd63d7f448f5394524c8d30c3b6576515
abu al ameen <abulyaqs@gmail.com>
2024-04-16 13:47:44 +0100

    class template for authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+# api/v1/auth/auth.py
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ Class to manage API authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Checks if authentication is required for the given path """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Gets the Authorization header from the request """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Gets the current user from the request """
+        return None
