448a71840a63049e53dd6797729d4b2e1671a9e2
abu al ameen <abulyaqs@gmail.com>
2024-04-16 20:38:53 +0100

    new task 4 and 5

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,8 +6,24 @@ this module is the basic auth class
 
 
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
     """this class the basic auth"""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ this function return base64 string """
+        if authorization_header is None or not \
+                isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        credentials = authorization_header.split(' ')
+        if len(credentials) != 2:
+            return None
+        # try:
+            # return base64.b64decode(credentials[1]).decode('utf-8')
+        # except binascii.Error:
+        return credentials[1]
