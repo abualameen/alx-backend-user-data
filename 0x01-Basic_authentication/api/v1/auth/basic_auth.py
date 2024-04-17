c59ab0602ea3e40bba4221ed178845f0711d3698
abu al ameen <abulyaqs@gmail.com>
2024-04-17 18:53:09 +0100

    correction for task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,7 +58,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """this function checks user obj from creden"""
-        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+        if user_email is None or not isinstance(user_email, str) or \
+                user_pwd is None or not isinstance(user_pwd, str):
             return None
         user_list = User.search({'email': user_email})
         if not user_list:
